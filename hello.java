c054e0df098be467ec12289ad5604eb56682d8a1
kirubhashakthi <kirubhashakthi@gmail.com>
2024-08-27 20:25:08 +0530

    java

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class hello{
+    public static void main (String args[])
+    {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    String b = sc.nextLine();
+    System.out.print(a+b);
+    }
+}
